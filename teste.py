899ba01f5cb891d5ef03e5766f8dec896d589e88
Nickolas <nickolasdionizio@hotmail.com>
2024-01-05 21:44:06 -0300

    Teste de commit via automacao

--- teste.py
+++ teste.py
@@ -4,6 +4,15 @@ repo = git.Repo('./')
 
 repo.index.add("*")
 
+nome_branch_local = repo.active_branch.name
+
+try:
+    repo.remotes.origin.refs[nome_branch_local]
+except IndexError:
+    branch_remota = "dev"
+
+    repo.git.branch(f"--set-upstream-to={branch_remota}", nome_branch_local)
+
 repo.index.commit("Teste de commit via automacao")
 
 repo.remotes.origin.push()
